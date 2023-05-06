e0662799e9a2dbd552ffb441af4d69357084e988
HTY <1044213317@qq.com>
2023-05-06 21:28:54 +0800

    fix start & end

--- envs/mpe/mapf.py
+++ envs/mpe/mapf.py
@@ -87,20 +87,20 @@ class Scenario:
         random_flag = True
         for i, (agent, landmark) in enumerate(zip(world.agents, world.landmarks)):
             # # 智能体起点按序均匀分布
-            # delta_x = width / (num_agents + 1)
-            # agent.pos = np.array([delta_x * (i + 1), agent.size])  # bottom
+            delta_x = width / (num_agents + 1)
+            agent.pos = np.array([delta_x * (i + 1), agent.size])  # bottom
 
-            # 起点随机
-            while True:
-                x = np_random.uniform(agent.size, width - agent.size)
-                success = True
-                for a in world.agents:
-                    if a.pos is not None and np.abs(x - a.pos[0]) < agent.size + a.size:
-                        success = False
-                        break
-                if success is True:
-                    break
-            agent.pos = np.array([x, agent.size])  # bottom
+            # # 起点随机
+            # while True:
+            #     x = np_random.uniform(agent.size, width - agent.size)
+            #     success = True
+            #     for a in world.agents:
+            #         if a.pos is not None and np.abs(x - a.pos[0]) < agent.size + a.size:
+            #             success = False
+            #             break
+            #     if success is True:
+            #         break
+            # agent.pos = np.array([x, agent.size])  # bottom
 
             agent.vel = np.zeros(world.dim_p)
             agent.c = np.zeros(world.dim_c)
@@ -108,27 +108,27 @@ class Scenario:
 
             # 避免终点重合
 
-            while True:
-                x = np_random.uniform(landmark.size, width - landmark.size)
-                success = True
-                for l in world.landmarks:
-                    if l.pos is not None and np.abs(x - l.pos[0]) < landmark.size + l.size:
-                        success = False
-                        break
-                if success is True:
-                    break
-            landmark.pos = np.array([x, height - landmark.size])  # top
+            # while True:
+            #     x = np_random.uniform(landmark.size, width - landmark.size)
+            #     success = True
+            #     for l in world.landmarks:
+            #         if l.pos is not None and np.abs(x - l.pos[0]) < landmark.size + l.size:
+            #             success = False
+            #             break
+            #     if success is True:
+            #         break
+            # landmark.pos = np.array([x, height - landmark.size])  # top
 
-            # if random_flag is False:
-            #     landmark.pos = np.array([delta_x * (random_index[i - 1] + 1), height - landmark.size])
-            #     random_flag = True
-            # elif random_index[i] == i and i != num_agents - 1:
-            #     landmark.pos = np.array([delta_x * (random_index[i + 1] + 1), height - landmark.size])
-            #     random_flag = False
-            # elif i == num_agents - 1:
-            #     landmark.pos = np.array([delta_x * (random_index[i] + 1) + 0.1, height - landmark.size])
-            # else:
-            #     landmark.pos = np.array([delta_x * (random_index[i] + 1), height - landmark.size])
+            if random_flag is False:
+                landmark.pos = np.array([delta_x * (random_index[i - 1] + 1), height - landmark.size])
+                random_flag = True
+            elif random_index[i] == i and i != num_agents - 1:
+                landmark.pos = np.array([delta_x * (random_index[i + 1] + 1), height - landmark.size])
+                random_flag = False
+            elif i == num_agents - 1:
+                landmark.pos = np.array([delta_x * (random_index[i] + 1) + 0.1, height - landmark.size])
+            else:
+                landmark.pos = np.array([delta_x * (random_index[i] + 1), height - landmark.size])
 
             landmark.vel = np.zeros(world.dim_p)
             landmark.color = agent.color
