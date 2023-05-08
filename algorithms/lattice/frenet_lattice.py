28620aa55b45908af7a5c3b4dc923f68dc0f9418
HTY <1044213317@qq.com>
2023-05-08 16:34:51 +0800

    render mapf

--- algorithms/lattice/frenet_lattice.py
+++ algorithms/lattice/frenet_lattice.py
@@ -42,17 +42,17 @@ class FrenetPath:
 
 class LatticePlanner:
     # Parameter
-    MAX_V = 8   # maximum speed [m/s]
-    MIN_V = 1   # minimum speed [m/s]
+    MAX_V = 5   # maximum speed [m/s]
+    MIN_V = 2   # minimum speed [m/s]
     DELTA_V = 1 # speed sampling length [m/s]
 
-    MAX_D = 4.0     # maximum road width [m]
-    MIN_D = -4.0    # minimun road width [m]
+    MAX_D = 2.0     # maximum road width [m]
+    MIN_D = -2.0    # minimun road width [m]
     DELTA_D = 1.0   # road width sampling length [m]
 
-    MAX_T = 8.0  # max prediction time [s]
-    MIN_T = 1.0  # min prediction time [s]
-    DELTA_T = 1  # time tick [s]
+    MAX_T = 6.0  # max prediction time [s]
+    MIN_T = 2.0  # min prediction time [s]
+    DELTA_T = 2  # time tick [s]
 
     MAX_S = 30.0  # max longitude distance [m]
     MIN_S = 10.0  # min longitude distance [m]
